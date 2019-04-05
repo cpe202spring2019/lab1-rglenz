a1a0f506304ef403338c41606d8357e5267f93db
rglenz <rglenz@csc303x30.ad.calpoly.edu>
2019-04-05 14:37:42 -0700

    final

--- location.py
+++ location.py
@@ -9,9 +9,9 @@ class Location:
 
 # ADD BOILERPLATE HERE (__eq__ and __repr__ functions)
     def __eq__(self,other):
-        return(self==other)
+        return(type(other)==Location and self.name==other.name and self.lat==other.lat and self.lon==other.lon)
     def __repr__(self):
-        return('Location('+self.name +', '+str(self.lat)+', '+str(self.lon)+')')
+        return("Location('"+self.name+"'" +', '+str(self.lat)+', '+str(self.lon)+')')
 def main():
     loc1 = Location("SLO", 35.3, -120.7)
     loc2 = Location("Paris", 48.9, 2.4)
