8714af1515338ad9e924208cdde0179ce91c4122
Raymond Lenz <raymondlenz@Raymonds-MacBook.local>
2019-04-10 18:16:58 -0700

    updated

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -5,14 +5,21 @@ from lab1 import *
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        #Checks max list if the list is empty 
+        #Checks max list if the list is None 
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+    def tes_max_list_iter_empty(self):
+        #checks empty lists 
+        tlist=[]
+        self.assertEqual(max_list_iter(tlist), None )
+
     def test_max_list_iter_same_max(self):
         #this checks that the max function will still run even if the max is repeated
         tlist=[1,2,3,0,3]
         self.assertEqual(max_list_iter(tlist), 3 )
+        tlist=[1,1,1]
+        self.assertEqual(max_list_iter(tlist), 1 )
     def test_max_list_iter_repeated_max(self):
         #checks if it will still run even if max is right next to an equal max
         tlist=[1,2,3,3]
@@ -30,18 +37,25 @@ class TestLab1(unittest.TestCase):
         tlist=[-1.1,-.2,-.3]
         self.assertEqual(max_list_iter(tlist), -.2 )
 
-    def test_reverse_rec_empty(self):
+    def test_reverse_rec_none(self):
         #checks is it will catch the empty list 
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             reverse_rec(tlist)
+    def test_reverse_rec_empty(self):
+        tlist=[]
+        self.assertEqual(reverse_rec(tlist),None)
     def test_reverse_rec_(self):
         #checks if it will still reverse correctly with floats and negative numbers
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+        self.assertEqual(reverse_rec([0,0,0]),[0,0,0])
         self.assertEqual(reverse_rec([1.1,30.5,5]),[5,30.5,1.1])
         self.assertEqual(reverse_rec([-1,-2,-3]),[-3,-2,-1])
         self.assertEqual(reverse_rec([-1.1,-30.5,-5]),[-5,-30.5,-1.1])
-        
+    def test_reverse_rec_letters(self):
+        #check if it still works with strings 
+        self.assertEqual(reverse_rec(['R','A','Y']),["Y",'A','R'])
+
     def test_bin_search(self):
         #Test all values from 0-10
         list_val =[0,1,2,3,4,7,8,9,10]
@@ -59,15 +73,25 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(bin_search(8, low, high, list_val), 6 )
         self.assertEqual(bin_search(9, low, high, list_val), 7)
         self.assertEqual(bin_search(10, low, high, list_val), 8)
-        
+    def test_bin_search_not_covered(self):
+        #checks if high and low doesn't span the list 
+        list_val =[0,1,2,3,4,7,8,9,10]
+        self.assertEqual(bin_search(2, 0, 3, list_val), 2 )
+        self.assertEqual(bin_search(4, 1, 3, list_val), None )
+        self.assertEqual(bin_search(9, 3, 8, list_val), 7 )
+        self.assertEqual(bin_search(8, 4, 7, list_val), 6 )
     def test_bin_search_high_is_lower(self):
+        #Cheks when the high is less than the low
         list_val =[0,1,2,3,4,7,8,9,10]
         self.assertEqual(bin_search(4, 9, 0, list_val), None )
         self.assertEqual(bin_search(4, 1, 0, list_val), None )
-    def test_bin_search_empty(self):
+    def test_bin_search_none(self):
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             bin_search(0,0,10,tlist)
+    def test_bin_search_empty(self): 
+        list_val=[]
+        self.assertEqual(bin_search(4, 1, 0, list_val), None )
 if __name__ == "__main__":
         unittest.main()
 
